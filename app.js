cc1106c3960c2dfdb0311237dcdb9cbb1e8b54ef
hesham.saleh.mohammed <hesham.saleh.mohammed@gmail.com>
2022-07-16 07:36:17 +0200

    static pages and route

--- app.js
+++ app.js
@@ -6,13 +6,57 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-
+const productRouter = express.Router();
 // middleware
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+productRouter.route('/').get((req, res) => {
+  res.render('products', {
+    data: [
+      {
+        Id: 1,
+        Name: 'p1',
+        Description: 'p1',
+        Image: 'p1.jpeg',
+      },
+      {
+        Id: 2,
+        Name: 'p2',
+        Description: 'p2',
+        Image: 'p2.jpeg',
+      },
+      {
+        Id: 3,
+        Name: 'p3',
+        Description: 'p3',
+        Image: 'p3.jpeg',
+      },
+    ],
+  });
+});
+
+productRouter.route('/1').get((req, res) => {
+  res.render('product', {
+    data: {
+      Id: 1,
+      Name: 'p1',
+      Description: 'p1',
+      Image: 'p1.jpeg',
+    },
+  });
+});
+
+app.use('/products', productRouter);
+
 app.get('/', (req, res) => {
-  res.send('Hello from my app');
+  res.render('index', {
+    title: 'ETSHO',
+    data: ['a', 'b', 'c'],
+  });
 });
 
 app.listen(PORT, () => {
