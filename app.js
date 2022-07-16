cbf9aaae2f37439c7a32f73277d9c2d0deee3b7b
hesham.saleh.mohammed <hesham.saleh.mohammed@gmail.com>
2022-07-16 05:35:13 +0200

    the first page

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+
+// middleware
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
+
+app.get('/', (req, res) => {
+  res.send('Hello from my app');
+});
+
+app.listen(3000, () => {
+  debug(`listening on port ${chalk.green('3000')}`);
+});
