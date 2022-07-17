56f7df1d4d54dfa64a204493b5e7d16917910a3d
hesham.saleh.mohammed <hesham.saleh.mohammed@gmail.com>
2022-07-17 05:36:02 +0200

    add rest apis

--- src/restApis/productsApiRouter.js
+++ src/restApis/productsApiRouter.js
@@ -6,6 +6,24 @@ const {MongoClient, ObjectId} = require('mongodb');
 const url =
   'mongodb+srv://heshamUser:172839@productsengine.atexpeh.mongodb.net/?retryWrites=true&w=majority';
 const dbName = 'productsEngine';
+
+router.get('/', function (req, res, next) {
+  const id = req.params.id;
+
+  (async function mongo() {
+    let client;
+    try {
+      client = await MongoClient.connect(url);
+      debug('Connected to Mongo DB');
+      const db = client.db(dbName);
+      const products = await db.collection('products').find().toArray();
+      res.json(products);
+    } catch (error) {
+      debug(error.stack);
+    }
+  })();
+});
+
 router.get('/:id', function (req, res, next) {
   const id = req.params.id;
 
@@ -19,6 +37,30 @@ router.get('/:id', function (req, res, next) {
         .collection('products')
         .findOne({_id: new ObjectId(id)});
       res.json(product);
+    } catch (error) {
+      debug(error.stack);
+    }
+  })();
+});
+
+router.post('/', function (req, res, next) {
+  const {name, description} = req.body;
+
+  (async function mongo() {
+    let client;
+    try {
+      client = await MongoClient.connect(url);
+      debug('Connected to Mongo DB');
+      const db = client.db(dbName);
+      const result = await db
+        .collection('products')
+        .insertOne({Name: name, Description: description, Image: 'p1.jpeg'});
+      res.json({
+        _id: result.insertedId,
+        Name: name,
+        Description: description,
+        Image: 'p1.jpeg',
+      });
     } catch (error) {
       debug(error.stack);
     }
